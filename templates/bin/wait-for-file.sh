2064140ad86616d16f76693f5ecc99ee280c76de
Suguru Hirahara <acioustick@noreply.codeberg.org>
2025-07-09 00:31:08 +0900

    Replace hard tabs with soft ones
    Signed-off-by: Suguru Hirahara <acioustick@noreply.codeberg.org>

--- templates/bin/wait-for-file.sh
+++ templates/bin/wait-for-file.sh
@@ -9,20 +9,20 @@ max_iterations=${2:-0}
 
 counter=0
 while ! [ -f $path ]; do
-		let counter=counter+1
+    let counter=counter+1
 
-		if [ "$max_iterations" -gt "0" ]; then
-				if [ "$counter" -gt "$max_iterations" ]; then
-						echo "Giving up waiting for $path after $max_iterations iterations"
-						exit 1
-				fi
-		fi
+    if [ "$max_iterations" -gt "0" ]; then
+        if [ "$counter" -gt "$max_iterations" ]; then
+            echo "Giving up waiting for $path after $max_iterations iterations"
+            exit 1
+        fi
+    fi
 
-		if [ "$max_iterations" -gt "0" ]; then
-			echo "$path is missing.. Waiting ($counter/$max_iterations)..."
-		else
-			echo "$path is missing.. Waiting ($counter/inf.)..."
-		fi
+    if [ "$max_iterations" -gt "0" ]; then
+    	echo "$path is missing.. Waiting ($counter/$max_iterations)..."
+    else
+    	echo "$path is missing.. Waiting ($counter/inf.)..."
+    fi
 
-		sleep 1
+    sleep 1
 done
